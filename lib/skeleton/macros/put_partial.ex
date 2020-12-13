2bb280c503c7b6dcd2108a56c79aa681a4127282
ricardo <>
2020-12-13 14:55:50 +0100

    put_ifmatch + refact of put_partial_types

--- lib/skeleton/macros/put_partial.ex
+++ lib/skeleton/macros/put_partial.ex
@@ -26,6 +26,7 @@ defmodule MapSchema.PutPartial do
       Put a new value in each field of the update map,  in the field of the object.
 
       But before of update the values always will be check the type.
+      If a field dont exist in the schema throw exception. (If you need be less strict you can use `put_ifmatch/1` or `put_ifmatch/2`)
       """
       def unquote(:put)(var!(map_update)) when is_map(var!(map_update)) do
         put(%{}, var!(map_update))
@@ -35,12 +36,32 @@ defmodule MapSchema.PutPartial do
       Put a new value in each field of the update map,  in the field of the object.
 
       But before of update the values always will be check the type.
+      If a field dont exist in the schema throw exception. (If you need be less strict you can use `put_ifmatch/1` or `put_ifmatch/2`)
       """
       def unquote(:put)(var!(mapa), var!(map_update)) when is_map(var!(map_update)) do
-        #PutPartial.put(__MODULE__, var!(mapa), var!(map_update))
         var!(custom_types) = schema_types()
         PutPartialTypes.put(__MODULE__, var!(mapa), var!(map_update), var!(custom_types))
       end
+
+      @doc """
+      Put a new value in each field of the update map, in the field of the object only if exist the field in the schema (ifmatch)
+
+      But before of update the values always will be check the type.
+      """
+      def unquote(:put_ifmatch)(var!(map_update)) when is_map(var!(map_update)) do
+        put_ifmatch(%{}, var!(map_update))
+      end
+
+      @doc """
+      Put a new value in each field of the update map, in the field of the object only if exist the field in the schema (ifmatch)
+
+      But before of update the values always will be check the type.
+      """
+      def unquote(:put_ifmatch)(var!(mapa), var!(map_update)) when is_map(var!(map_update)) do
+        var!(custom_types) = schema_types()
+        PutPartialTypes.put_ifmatch(__MODULE__, var!(mapa), var!(map_update), var!(custom_types))
+      end
+
     end
   end
 
--- lib/skeleton/methods/put_partial_types.ex
+++ lib/skeleton/methods/put_partial_types.ex
@@ -6,7 +6,92 @@ defmodule MapSchema.Methods.PutPartialTypes do
   alias MapSchema.Types
   alias MapSchema.Utils
 
+  alias MapSchema.Methods.PutPartialTypes
+
+  defstruct module: nil,
+            map: nil,
+            map_update: nil,
+            custom_types: nil,
+            schema: nil,
+            lista_fields: [],
+            emit_exception: true
+
   def put(module, map, map_update, custom_types) do
+    emit_exception = true
+    action = action_contructor(module, map, map_update, custom_types, emit_exception)
+    execute_put(action)
+  end
+
+  def put_ifmatch(module, map, map_update, custom_types) do
+    emit_exception = false
+    action = action_contructor(module, map, map_update, custom_types, emit_exception)
+    execute_put(action)
+  end
+
+  defp action_contructor(module, map, map_update, custom_types, emit_exception) do
+    schema = apply(module, :schema, [])
+
+    %PutPartialTypes{
+      module: module,
+      map: map,
+      map_update: map_update,
+      custom_types: custom_types,
+      schema: schema,
+      lista_fields: [],
+      emit_exception: emit_exception
+    }
+  end
+
+  def execute_put(action) do
+    action.map_update
+    |> Map.keys()
+    |> Enum.reduce(action.map, fn(field, acc_map) ->
+      valor = get_in(action.map_update, [field])
+      type = get_in(action.schema, [field])
+      lista_fields = action.lista_fields ++ [field]
+
+      cond do
+        type == nil ->
+          if action.emit_exception do
+            throw("Error schema: the field " <> field <> " dont exit in schema")
+          else
+            acc_map
+          end
+        Types.is_flexible_nested?(action.custom_types, type) ->
+          acc_map
+          |> call_put_of_field(action.module, lista_fields, valor)
+        is_map(type) ->
+          sub_schema = get_in(action.schema, [field])
+          sub_map_update = get_in(action.map_update, [field])
+
+          action
+          |> update_action(%{
+            map: acc_map,
+            schema: sub_schema,
+            map_update: sub_map_update,
+            lista_fields: lista_fields
+          })
+          |> execute_put()
+        true ->
+          acc_map
+          |> call_put_of_field(action.module, lista_fields, valor)
+      end
+    end)
+  end
+
+  defp update_action(action, changes) do
+    changes
+    |> Map.keys()
+    |> Enum.reduce(action, fn(key, acc_action) ->
+      value = Map.get(changes, key)
+
+      acc_action
+      |> Map.put(key, value)
+    end)
+  end
+
+
+  def put2(module, map, map_update, custom_types) do
     schema = apply(module, :schema, [])
     put(map, module, schema, map_update, custom_types, [])
   end
