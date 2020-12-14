1db69c09dac17c390b7bfeb4c7bb1a30f2b33145
ricardo <>
2020-12-14 10:56:44 +0100

    conflict methods colisions, sorry

--- lib/skeleton/macros/check.ex
+++ lib/skeleton/macros/check.ex
@@ -3,8 +3,10 @@ defmodule MapSchema.Macros.Check do
   @doc """
   The Check module compone the macros that let us, build the methods is_valid?:
 
-  `is_valid?(map)`
+  `is_map_valid?(map)`
 
+  Sorry I need rename this method because I would like make compatible a mapschema
+  with customtype.
   """
   def install do
     build_check()
@@ -15,7 +17,7 @@ defmodule MapSchema.Macros.Check do
       @doc """
       It´s will return true, if the map follow the schema.
       """
-      def unquote(:is_valid?)(var!(mapa)) do
+      def unquote(:is_map_valid?)(var!(mapa)) do
         put(%{}, var!(mapa))
         true
       catch
