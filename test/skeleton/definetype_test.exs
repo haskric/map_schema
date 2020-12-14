3dc66475ae64d5bb6412928d1b3c36f8600a72e9
ricardo <>
2020-12-14 18:06:45 +0100

    testing recursive type list_items

--- test/skeleton/definetype_test.exs
+++ test/skeleton/definetype_test.exs
@@ -4,7 +4,6 @@ defmodule MapSchema.DefineTypeTest do
 
   alias MapSchema.Exceptions
 
-
   test "Recursive definition MapSchema" do
 
     defmodule Item do
@@ -20,7 +19,28 @@ defmodule MapSchema.DefineTypeTest do
         }
     end
 
-    IO.inspect Item.schema_types()
+    item_with_others = Item.new()
+      |> Item.put_name("container")
+      |> Item.put_list_items([
+        %{"name"=> "item 1"}, %{"name"=> "item 2"}
+      ])
+
+    assert Item.get_name(item_with_others) == "container"
+    assert Item.get_list_items(item_with_others) == [
+      %{"name"=> "item 1"}, %{"name"=> "item 2"}
+    ]
+
+    try do
+      Item.mut_list_items(item_with_others, fn(old_list) ->
+        old_list ++ [%{"name"=> :invalid_type}] end)
+    catch
+      e ->
+        ## Expected something similar to:
+
+        ## "Casting error: We can´t cast the value of \"list_items\" to \"<list_items>\" type.
+        ## Review the type it please."
+        assert e == Exceptions.cast_error("list_items", "<list_items>")
+    end
 
   end
 
--- test/skeleton/definition_custom_type_test.exs
+++ test/skeleton/definition_custom_type_test.exs
@@ -1,7 +1,5 @@
 defmodule MapSchema.DefinitionCustomTypeTest do
   @moduledoc false
   use ExUnit.Case
-  alias MapSchema.Exceptions
-
 
 end
